fcf1197f9f71004eb6b32e393ce17368e572de00
darianthao <justdarian01@gmail.com>
2022-12-27 10:15:43 -0600

    Installed MaterialUI (unsure if going to use) and created Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import './Hero.css'
+
+function Hero() {
+    return (
+        <div className="Hero">
+            <div className="Hero-Text">
+                <h1>iBulky</h1>
+                <div className="Title-Subtitle">Having a hard time gaining weight?</div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
