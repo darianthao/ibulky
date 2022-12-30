6764cfd4a7e35696acb75cf2e1aa004056f2b06b
darianthao <justdarian01@gmail.com>
2022-12-30 15:51:43 -0600

    finished carbohydrate section with styling

--- src/components/Description/Carbohydrates/Carbohydrates.js
+++ src/components/Description/Carbohydrates/Carbohydrates.js
@@ -1,9 +1,15 @@
 import './Carbohydrates.css'
+import boltLogo from '../../../images/bolt.png'
+import breadLogo from '../../../images/bread.png'
 
 function Carbohydrates() {
     return (
         <>
             <div className="carbohydrates-context-container">
+                <div className="carbohydrates-images">
+                    <img src={boltLogo} alt="bolt logo" className="boltLogo"></img>
+                    <img src={breadLogo} alt="bread logo" className="breadLogo"></img>
+                </div>
                 <div className="carbohydrates-description">
                     <h1>Carbohydrates are your body's fuel</h1>
                     <p>What are <strong>carbohydrates?</strong> Carbohydrates are found
